0337cd87e930e631c35cccfc0c9aa5eb1af6a6b8
wutianyang <1582869895@qq.com>
2021-03-26 18:20:23 +0800

    添加侧边栏

--- app/src/main/java/com/example/wanandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/wanandroid/ui/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.wanandroid.ui.main
 
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.wanandroid.R
 import com.example.wanandroid.base.BaseActivity
 import com.example.wanandroid.databinding.MainActivityBinding
 import com.example.wanandroid.utils.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +18,21 @@ class MainActivity : BaseActivity<MainActivityBinding>() {
     override val layoutId: Int get() = R.layout.main_activity
 
     override fun initView() {
+        mBinding.toolbar.run {
+            setSupportActionBar(this)
+        }
+        mBinding.drawerLayout.run {
+            val toggle = ActionBarDrawerToggle(this@MainActivity,
+                    this,
+                    mBinding.toolbar,
+                    R.string.navigation_drawer_open,
+                    R.string.navigation_drawer_close)
+            addDrawerListener(toggle)
+            toggle.syncState()
+        }
+        mBinding.navView.run {
+            setNavigationItemSelectedListener(onDrawerNavigationItemSelectedListener)
+        }
         setupBottomNavigationBar()
     }
 
@@ -29,4 +48,18 @@ class MainActivity : BaseActivity<MainActivityBinding>() {
             setupActionBarWithNavController(navController)
         })
     }
+
+    private val onDrawerNavigationItemSelectedListener =
+            NavigationView.OnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.nav_like -> {
+
+                    }
+                    R.id.nav_about -> {
+
+                    }
+                }
+                mBinding.drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
 }
